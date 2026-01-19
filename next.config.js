3e0f4181eff44f7a4fd8ba1f0a225efa1a081dc1
Reymart Militante <112607511+martmilitante@users.noreply.github.com>
2026-01-19 22:29:16 +0800

    Update next.config.js to set basePath and assetPrefix

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  output: 'export',
+  basePath: '/portfolio',
+  assetPrefix: '/portfolio',
+  images: {
+    unoptimized: true,
+  },
+  env: {
+    NEXT_PUBLIC_BASE_PATH: '/portfolio'
+  }
+}
 
 module.exports = nextConfig
